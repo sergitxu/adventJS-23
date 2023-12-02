a4e49126a5554a209ac3ddb4addb00bb50b6464e
sergitxu <sergiortiz@gmail.com>
2023-12-02 12:51:06 +0100

    Solve 01

---
+++ 01/01.js
@@ -0,0 +1,16 @@
+function findFirstRepeated(gifts) {
+    const toCompare = new Set();
+    for (const gift of gifts) {
+        if (toCompare.has(gift)) {
+            return gift;
+        }
+        toCompare.add(gift);
+    }
+    return -1;
+}
+
+// Some tests
+console.log('expected 7: ' + findFirstRepeated([5, 1, 7, 3, 7, 5, 1]));
+console.log('expected -1: ' + findFirstRepeated([]))
+console.log('expected -1: ' + findFirstRepeated([1, 2]))
+console.log('expected 2: ' + findFirstRepeated([2, 2]))

e6123d704c51af0114039b6e619e03baa836e0c4
sergitxu <sergiortiz@gmail.com>
2023-12-03 14:38:18 +0100

    Solve 03

---
+++ 03 El elfo travieso/03.js
@@ -0,0 +1,16 @@
+function findNaughtyStep(original, modified) {
+    for (let i = 0; i <= original.length; i++) {
+        if (original[i] !== modified[i]) {
+            if (original[i] === modified[i + 1]) {
+                return modified[i];
+            }
+            return original[i];
+        }
+    }
+    return '';
+}
+
+console.log(`Expected 'e': ${findNaughtyStep('abcd', 'abcde')}`); // 'e'
+console.log(`Expected '': ${findNaughtyStep('abcde', 'abcde')}`); // ''
+console.log(`Expected 'f': ${findNaughtyStep('stepfor', 'stepor')}`); // 'f'
+console.log(`Expected 'o': ${findNaughtyStep('xxxx', 'xxoxx')}`); // 'o'

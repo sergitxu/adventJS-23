9a167205153c4c2f1c117476a1bcbe2b6fe99ce6
sergitxu <sergiortiz@gmail.com>
2023-12-04 00:11:37 +0100

    Add comments

--- 03 El elfo travieso/03.js
+++ 03 El elfo travieso/03.js
@@ -1,9 +1,13 @@
 function findNaughtyStep(original, modified) {
     for (let i = 0; i <= original.length; i++) {
+        // Recorremos los caracteres de original más uno por si hay un paso añadido
         if (original[i] !== modified[i]) {
+            // Ha habido un paso cambiado
             if (modified.length > original.length) {
+                // se ha añadido un paso al original
                 return modified[i];
             }
+            // se ha eliminado un paso al original
             return original[i];
         }
     }

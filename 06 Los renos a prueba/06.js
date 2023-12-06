7f345f3fcca6801b0dd6ae593460d13af88227a0
sergitxu <sergiortiz@gmail.com>
2023-12-06 17:53:05 +0100

    Solve 06

---
+++ 06 Los renos a prueba/06.js
@@ -0,0 +1,27 @@
+function maxDistance(movements) {
+    let maxNumber = 0;
+    let extra = 0;
+    for (let movement of movements) {
+        if (movement === '>') {
+            maxNumber++;
+        } else if (movement === '<') {
+            maxNumber--;
+        } else {
+            extra++
+        }
+    };
+    return Math.abs(maxNumber) + extra;
+}
+
+
+const movements = '>>*<'
+const result = maxDistance(movements)
+console.log(`Expected: 2: ${result}`) // -> 2
+
+const movements2 = '<<<>'
+const result2 = maxDistance(movements2)
+console.log(`Expected: 2: ${result2}`) // -> 2
+
+const movements3 = '>***>'
+const result3 = maxDistance(movements3)
+console.log(`Expected: 5: ${result3}`) // -> 5

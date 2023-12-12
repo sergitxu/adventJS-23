c3ca0e1ae801cdf61382c12e8287782d8df60b50
sergitxu <sergiortiz@gmail.com>
2023-12-12 22:16:07 +0100

    Update 12

--- 12 copia valida/12.js
+++ 12 copia valida/12.js
@@ -3,17 +3,6 @@ function checkIsValidCopy(original, copy) {
     const lowerLetters = new RegExp('[a-z]');
     const specialCharacters = new RegExp('[#+:. ]');
 
-    function checkSpecialCharacter(original, copy) {
-        let reference = '#+:. ';
-        let index = reference.indexOf(original);
-        let possibleRef = reference.substring[index, reference.length];
-        const possibleSpecialCharacters = new RegExp('[${possibleRef}]');
-        if (possibleRef && copy.match(possibleSpecialCharacters)) {
-            return true;
-        }
-        return false;
-    }
-
     if (original.length !== copy.length) {
         return false;
     };
@@ -33,16 +22,33 @@ function checkIsValidCopy(original, copy) {
             if (originalCharacter === ' ' && copyCharacter !== ' ') {
                 return false;
             }
-            checkSpecialCharacter(originalCharacter, copyCharacter);
+            return checkSpecialCharacter(originalCharacter, copyCharacter);
         } else if (originalCharacter !== copyCharacter) {
             return false;
         }
+    }
+    /* 
+    Without this function the code still works 
+    because there are no examples with '#+:.' characters 
+    in the original message
+    */
+    function checkSpecialCharacter(original, copy) {
+        let reference = '#+:. ';
+        let index = reference.indexOf(original);
+        console.log(`original: ${original}, copy ${copy}`)
+        let possibleRef = reference.substring(index, reference.length);
+        const possibleSpecialCharacters = new RegExp(`[${possibleRef}]`);
+        console.log(`possibleSpecialCharacters: ${possibleSpecialCharacters}`);
+        if (possibleRef && copy.match(possibleSpecialCharacters)) {
+            return true;
+        }
+        return false;
     }
     return true;
 
 }
 console.log(`Expected: false - ${checkIsValidCopy('Santa Claus', 's#+:.#c:. s')}`);
-console.log(`Expected: true - ${checkIsValidCopy('3 #egalos', '3 .+:# #:')}`);
+console.log(`Expected: true FALSE - ${checkIsValidCopy('3: #egalos', '3+ .+:# #:')}`);
 console.log(`Expected: true - ${checkIsValidCopy('Santa Claus', '###:. c:+##')}`);
 console.log(`Expected: false - ${checkIsValidCopy('SAOPO]', 's')}`);
 console.log(`Expected: true - ${checkIsValidCopy(

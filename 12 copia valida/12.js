8f29f3ba14bef08d7355b1de65f77dcfb47df12a
sergitxu <sergiortiz@gmail.com>
2023-12-13 11:09:45 +0100

    Improve 12

--- 12 copia valida/12.js
+++ 12 copia valida/12.js
@@ -1,51 +1,34 @@
 function checkIsValidCopy(original, copy) {
     const upperLetters = new RegExp('[A-Z]');
     const lowerLetters = new RegExp('[a-z]');
-    const specialCharacters = new RegExp('[#+:. ]');
+    const otherChars = new RegExp('[#+:. ]');
 
     if (original.length !== copy.length) {
         return false;
     };
-
-    for (let i = 0; i < original.length; i++) {
-        const originalCharacter = original[i];
-        const copyCharacter = copy[i];
-        if (originalCharacter.match(upperLetters)) {
-            if (copyCharacter !== originalCharacter.toLowerCase() && !copyCharacter.match(specialCharacters)) {
+    original.split(' ');
+    for (let letter of original) {
+        const copyLetter = copy[original.indexOf(letter)];
+        // Original upperCase
+        if (letter.match(upperLetters)) {
+            if (copyLetter !== letter.toLowerCase() && !copyLetter.match(otherChars)) {
                 return false;
             }
-        } else if (originalCharacter.match(lowerLetters)) {
-            if (copyCharacter !== originalCharacter && !copyCharacter.match(specialCharacters)) {
+            // Original lowerCase
+        } else if (letter.match(lowerLetters)) {
+            if (copyLetter !== letter && !copyLetter.match(otherChars)) {
                 return false;
             }
-        } else if (originalCharacter.match(specialCharacters)) {
-            if (originalCharacter === ' ' && copyCharacter !== ' ') {
+            // Original otherChars
+        } else if (letter.match(otherChars)) {
+            if (letter === ' ' && copyLetter !== ' ') {
                 return false;
             }
-            return checkSpecialCharacter(originalCharacter, copyCharacter);
-        } else if (originalCharacter !== copyCharacter) {
+        } else if (letter !== copyLetter) {
             return false;
         }
     }
-    /* 
-    Without this function the code still works 
-    because there are no examples with '#+:.' characters 
-    in the original message
-    */
-    function checkSpecialCharacter(original, copy) {
-        let reference = '#+:. ';
-        let index = reference.indexOf(original);
-        console.log(`original: ${original}, copy ${copy}`)
-        let possibleRef = reference.substring(index, reference.length);
-        const possibleSpecialCharacters = new RegExp(`[${possibleRef}]`);
-        console.log(`possibleSpecialCharacters: ${possibleSpecialCharacters}`);
-        if (possibleRef && copy.match(possibleSpecialCharacters)) {
-            return true;
-        }
-        return false;
-    }
     return true;
-
 }
 console.log(`Expected: false - ${checkIsValidCopy('Santa Claus', 's#+:.#c:. s')}`);
 console.log(`Expected: true FALSE - ${checkIsValidCopy('3: #egalos', '3+ .+:# #:')}`);

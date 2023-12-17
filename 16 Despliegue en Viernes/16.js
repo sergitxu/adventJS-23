d9aa3a7ef6ed58f9a69a2adf5748ac25604349e0
Sergitxu <sergiortiz@gmail.com>
2023-12-17 19:52:07 +0100

    Add tests

--- 16 Despliegue en Viernes/16.js
+++ 16 Despliegue en Viernes/16.js
@@ -14,7 +14,138 @@ function transformTree(tree) {
 return buildTree(0);
 }
 
-console.log(
-    transformTree([3, 1, 0, 8, 12, null, 1])
-);
-// 
+// TESTS
+const assert = require('assert');
+
+try {
+    assert.deepStrictEqual(transformTree([3, 1, 0, 8, 12, null, 1]), 
+    {
+        value: 3,
+        left: {
+          value: 1,
+          left: {
+            value: 8,
+            left: null,
+            right: null
+          },
+          right: {
+            value: 12,
+            left: null,
+            right: null
+          }
+        },
+        right: {
+          value: 0,
+          left: null,
+          right: {
+            value: 1,
+            left: null,
+            right: null
+          }
+        }
+      }
+    );
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
+
+try {
+    assert.deepStrictEqual(transformTree([]), null);
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
+
+try {
+    assert.deepStrictEqual(transformTree([1]), 
+    {
+        "value": 1,
+        "left": null,
+        "right": null
+    });
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
+
+try {
+    assert.deepStrictEqual(transformTree([17, 0, null, null, 1]), 
+    {
+        "value": 17,
+        "left": {
+          "value": 0,
+          "left": null,
+          "right": {
+            "value": 1,
+            "left": null,
+            "right": null
+          }
+        },
+        "right": null
+      });
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
+
+try {
+    assert.deepStrictEqual(transformTree([1,2,3]), 
+    {
+        "value": 1,
+        "left": {
+          "value": 2,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "value": 3,
+          "left": null,
+          "right": null
+        }
+      });
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
+
+try {
+    assert.deepStrictEqual(transformTree([2, 7, 5, null, 6, null, 9, null, null, 1, 11, null, null, null, 10]), 
+    {
+        "value": 2,
+        "left": {
+          "value": 7,
+          "left": null,
+          "right": {
+            "value": 6,
+            "left": {
+              "value": 1,
+              "left": null,
+              "right": null
+            },
+            "right": {
+              "value": 11,
+              "left": null,
+              "right": null
+            }
+          }
+        },
+        "right": {
+          "value": 5,
+          "left": null,
+          "right": {
+            "value": 9,
+            "left": null,
+            "right": {
+              "value": 10,
+              "left": null,
+              "right": null
+            }
+          }
+        }
+      }
+      );
+    console.log('Test ok!');
+} catch (error) {
+    console.error('Failed test:', error);
+}
